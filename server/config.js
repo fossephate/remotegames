c6cf7b4ad7e81c4ff59ef4b33a8a7ebacba0a455
mfosse <matt.cfosse@gmail.com>
2018-06-11 14:21:40 +0000

    remove database & update secrets

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_SECRET: "aaaaaaaaa",
+  SESSION_SECRET: "thisTimeItsReallySecure",
   TWITCH_SECRET: "t9aa8z3fblunawznkgeyu5hp1b3q9z",
-  HASH_SECRET: "security!!!",
+  HASH_SECRET: "ImessedUpAgain",
 }
